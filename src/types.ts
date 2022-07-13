38d250f2795e109ed2de4cb51c289fc52403ffc5
larsvankleef <larsvankleef@outlook.com>
2022-07-13 16:59:15 +0200

    Fix incorect type of cache

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
 type Catchable<T> = T & { catch(callback: () => void): any }
 export type CacheParams = { allowReturnExpiredValue: boolean, expirationTime: number }
-export type Cache<T> = (props: { cacheKey: string | Array<any>, getValue: () => T | Catchable<T> }) => T
+export type Cache = <T>(props: { cacheKey: string | Array<any>, getValue: () => T | Catchable<T> }) => T
